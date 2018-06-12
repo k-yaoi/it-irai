84f77cfad3eba340b4a9b98122227e530bf388fe
k-yaoi <kaoru.yaoi@gmail.com>
2018-06-12 12:43:31 +0000

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # validationの設定
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  # パスワード暗号化
+  has_secure_password
+  
+  
+  
+  
+end
